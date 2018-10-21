a1e82c809d7dd172494e82b091f4acbb366c5bc5
Prabin Pun <prabin1punah@gmail.com>
2018-10-21 07:41:24 -0400

    Add project files.

---
+++ KeyValue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyValueStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+        }
+
+       
+    }
+    struct KeyValue
+    {
+        public string Key; 
+
+        public string Object;
+       
+
+    }
+}
